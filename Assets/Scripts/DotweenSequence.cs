137460483645f1395e4d70dcc36845080d083c48
Han-An <83206248+Han-An@users.noreply.github.com>
2025-03-21 00:03:21 +0900

    Ladder_3
    기능구현완
    버튼 안눌리는 예외처리, 결과창에서 처음으로 가기 버튼 구현, 경로 애니메이션 색상이랑 두께 변경 필요

--- Assets/Scripts/DotweenSequence.cs
+++ Assets/Scripts/DotweenSequence.cs
@@ -1,17 +1,83 @@
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class DotweenSequence : MonoBehaviour
 {
     public Transform target;
+    public Image logoImage;
+    public int resultNum;
+    public Image[] resultImages;
     public LineRenderer[] lineRenderers;
-    public TextMeshPro[] textObjects;
+    public TextMeshPro[] textObjects1;
+    public TextMeshPro[] textObjects2;
+    public TextMeshPro[] textObjects3;
+    public TextMeshPro[] textObjects4;
+    public TextMeshPro[] textObjects5;
+    public TextMeshPro[] textObjects6;
+    public TextMeshPro[] textObjects7;
+    public GameObject LadderLinerenderer;
+    public GameObject DrawLinerenderer;
+
+
+    
     public float speed;
+    public Button[] buttons;
     
     void Start()
     {
+        LadderLinerenderer.SetActive(true);
+        
+        buttons[0].onClick.AddListener(() =>
+        {
+            Generate(textObjects1);
+            resultNum = 1;
+        });
+        
+        buttons[1].onClick.AddListener(() =>
+        {
+            Generate(textObjects2);
+            resultNum = 2;
+        });
+        
+        buttons[2].onClick.AddListener(() =>
+        {
+            Generate(textObjects3);
+            resultNum = 3;
+        });
+        
+        buttons[3].onClick.AddListener(() =>
+        {
+            Generate(textObjects4);
+            resultNum = 4;
+        });
+        buttons[4].onClick.AddListener(() =>
+        {
+            Generate(textObjects5);
+            resultNum = 5;
+        });
+        buttons[5].onClick.AddListener(() =>
+        {
+            Generate(textObjects6);
+            resultNum = 6;
+        });
+        buttons[6].onClick.AddListener(() =>
+        {
+            Generate(textObjects7);
+            resultNum = 7;
+        });
+    }
+
+    private void Generate(TextMeshPro[] textObjects)
+    {
+        foreach (var button in buttons)
+        {
+            logoImage.enabled = true;
+            button.enabled = false;
+        }
+        
+        
         foreach (var line in lineRenderers)
         {
             line.gameObject.SetActive(false);
@@ -27,6 +93,7 @@ public class DotweenSequence : MonoBehaviour
         
         sequence.Play();
         
+        
         void Init (int index)
         {
             lineRenderers[index].gameObject.SetActive(true);
@@ -38,19 +105,58 @@ public class DotweenSequence : MonoBehaviour
         {
             float duration = Vector3.Distance(textObjects[index].transform.position, textObjects[index + 1].transform.position) / speed;
             sequence.Append(DOTween.To(
-                () => lineRenderers[index].GetPosition(1), x => lineRenderers[index].SetPosition(1, x), textObjects[index + 1].transform.position, duration)
-                .SetEase(Ease.Linear));
+                    () => lineRenderers[index].GetPosition(1), x => lineRenderers[index].SetPosition(1, x), textObjects[index + 1].transform.position, duration)
+                .SetEase(Ease.Linear).OnUpdate(() => 
+                    target.position = lineRenderers[index].GetPosition(1))
+            );
             sequence.AppendCallback(() =>
             {
                 Init(index + 1);
             });
+            
+            sequence.OnComplete(() => ResultImage());
         }
     }
     
-    void Update()
+   void ResultImage()
     {
+        logoImage.enabled = false;
+        LadderLinerenderer.SetActive(false);
+        DrawLinerenderer.SetActive(false);
+        Debug.Log("Result Image");
         
+        
+        switch (resultNum)
+        {
+            case 1:
+                resultImages[0].enabled = true;
+                Debug.Log("Result 1");
+                break;
+            case 2:
+                resultImages[1].enabled = true;
+                Debug.Log("Result 2");
+                break;
+            case 3:
+                resultImages[2].enabled = true;
+                Debug.Log("Result 3");
+                break;
+            case 4:
+                resultImages[3].enabled = true;
+                Debug.Log("Result 4");
+                break;
+            case 5:
+                resultImages[4].enabled = true;
+                Debug.Log("Result 5");
+                break;
+            case 6:
+                resultImages[5].enabled = true;
+                Debug.Log("Result 6");
+                break;
+            case 7:
+                resultImages[6].enabled = true;
+                Debug.Log("Result 7");
+                break;
+        }
     }
-    
    
 }
